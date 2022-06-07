91631e29bca97a13dd17eb73a08900d497f9b549
Adam Jelley <adjelley@gmail.com>
2022-06-07 12:04:26 +0100

    Fix imports, try obs wrappers and reformat

--- utils/env.py
+++ utils/env.py
@@ -1,8 +1,13 @@
 import gym
 import gym_minigrid
+from gym_minigrid.wrappers import *
 
 
 def make_env(env_key, seed=None):
     env = gym.make(env_key)
+    # env = FullyObsWrapper(env) # Get full grid (25 x 25 rather than 7 x 7)
+    # env = RGBImgPartialObsWrapper(env)  # Get pixel observations
+    # env = ImgObsWrapper(env)  # Get rid of the 'mission' field
+    # env = ActionBonus(env)
     env.seed(seed)
     return env
