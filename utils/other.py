34711711bff22526605df095bf45a02d68db5453
Adam Jelley <s2139934@edwards.inf.ed.ac.uk>
2022-08-09 12:08:33 +0100

    Add automatic GPU device selection and MPS support

--- utils/other.py
+++ utils/other.py
@@ -2,9 +2,22 @@ import random
 import numpy
 import torch
 import collections
+import os
+import GPUtil
 
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Set device
+if torch.cuda.is_available():
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    deviceID = GPUtil.getFirstAvailable(
+        order="load", maxLoad=0.4, maxMemory=0.4, attempts=1, interval=900, verbose=False
+    )[0]
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(deviceID)
+    device = torch.device(f"cuda:{deviceID}")
+elif torch.has_mps:
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
 
 
 def seed(seed):
